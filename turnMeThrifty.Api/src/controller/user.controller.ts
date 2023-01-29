ebbdf5dd1ee08edb367714fd1fc6647df0ce7c29
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2023-01-29 13:59:13 -0300

    feat: added user verification

--- turnMeThrifty.Api/src/controller/user.controller.ts
+++ turnMeThrifty.Api/src/controller/user.controller.ts
@@ -1,8 +1,8 @@
 import { debug } from 'console';
 import { Request, Response } from 'express'
 import i18next from 'i18next';
-import { CreateUserInput } from '../schema/user.schema';
-import { createUser } from '../service/user.service';
+import { CreateUserInput, VerifyUserInput } from '../schema/user.schema';
+import { createUser, findUserById } from '../service/user.service';
 import log from '../utils/logger';
 import sendEmail from '../utils/mailer';
 
@@ -15,12 +15,12 @@ export async function createUserHandler(
     try {
 
         const user = await createUser(body);
-        log.info("user createad -> " + user)
+
         await sendEmail({
             from: 'test@example.com',
             to: user.email,
-            subject: "Please verify your e-mail",
-            text: `Verification Code ${user.verificationCode}. Id: ${user._id}`
+            subject: i18next.t('user:verificatione_email_subject'),
+            text: `${i18next.t('user:verification_code')} ${user.verificationCode}. Id: ${user._id}`
         });
 
         return res.send(i18next.t('user:user_create_success'))
@@ -30,6 +30,36 @@ export async function createUserHandler(
             return res.status(409).send(i18next.t('user:account_exists'));
         }
 
+        return res.status(500).send(e);
+    }
+}
+
+export async function verifyUserHandler(req: Request<VerifyUserInput>, res: Response) {
+
+    try {
+        const id = req.params.id;
+        const verificationCode = req.params.verificationCode;
+
+        const user = await findUserById(id);
+
+        if (!user) {
+            return res.send(i18next.t('user:could_not_verify_user'));
+        }
+
+        if (user.verified) {
+            return res.send(i18next.t('user:user_already_verified'));
+        }
+
+        if (user.verificationCode === verificationCode) {
+            user.verified = true;
+
+            await user.save();
+
+            return res.send(i18next.t('user:user_verify_success'));
+        }
+
+        return res.send(i18next.t('user:could_not_verify_user'));
+    } catch (e: any) {
         return res.status(500).send(e);
     }
 }
