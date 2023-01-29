18eab7ee223908bccac66fd2bcbeb6ef6cc0e8fa
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2023-01-29 02:19:39 -0300

    feat: added i18next internationalization initialization, and some basic translations for en and pt

--- turnMeThrifty.Api/src/server.ts
+++ turnMeThrifty.Api/src/server.ts
@@ -4,12 +4,18 @@ import config from 'config'
 import { connectToDb } from './utils/connectToDb';
 import log from './utils/logger';
 import router from './routes'
+import { initializeInternationalization } from './utils/InitializeInternationalization';
+import i18nextMiddleware from "i18next-express-middleware"
+import i18next from 'i18next';
 
 const app: Express = express();
 
 const port = config.get('port');
 
 app.use(router);
+
+initializeInternationalization();
+app.use(i18nextMiddleware.handle(i18next));
 
 app.listen(port, () => {
     log.info(`[Server]: I am running at https://localhost:${port}`);
---
+++ turnMeThrifty.Api/src/utils/InitializeInternationalization.ts
@@ -0,0 +1,24 @@
+import i18next from "i18next";
+import i18nextMiddleware from "i18next-express-middleware"
+
+export function initializeInternationalization() {
+    i18next
+        .use(i18nextMiddleware.LanguageDetector)
+        .init({
+            fallbackLng: 'en',
+            preload: ['en', 'pt'],
+            saveMissing: true,
+            resources: {
+                en: {
+                    common: require('../locales/en/common.json'),
+                    error: require('../locales/en/error.json'),
+                    user: require('../locales/en/user.json')
+                },
+                pt: {
+                    common: require('../locales/pt/common.json'),
+                    error: require('../locales/pt/error.json'),
+                    user: require('../locales/en/user.json')
+                }
+            }
+        });
+}
