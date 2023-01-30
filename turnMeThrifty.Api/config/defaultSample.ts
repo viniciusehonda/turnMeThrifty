0f87205e0b1f507edfe1b5bfef9f6a1cdf492766
Vinicius Eidi Honda <viniciusehonda@gmail.com>
2023-01-30 00:00:28 -0300

    chore: added a sample for the default.ts config file

---
+++ turnMeThrifty.Api/config/defaultSample.ts
@@ -0,0 +1,14 @@
+// export default {
+//     port: 3000,
+//     dbUri: "",
+//     logLevel: "info",
+//     accessTokenPrivateKey: "",
+//     refreshTokenPrivateKey: "",
+//     smtp: {
+//         user: '',
+//         pass: '',
+//         host: '',
+//         port: 0,
+//         secure: false 
+//     }
+// }
